b5418318f36978bcbd16ba66bd003a0139a03bb6
Caio Montenegro <caio.almeida@portaldotrader.com.br>
2022-07-10 16:29:45 -0300

    Seteup and App builded

---
+++ Project 2/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+
+  </ul>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      storeResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official Vue.js documentatiom', 
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to Google', 
+          link: 'https://google.com'
+        },
+      ]
+    }
+  }
+}
+</script>
