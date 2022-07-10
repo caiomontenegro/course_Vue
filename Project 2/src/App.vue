3aa8f5fc4135fa8f1012910d8f6520269939a2dc
Caio Montenegro <caio.almeida@portaldotrader.com.br>
2022-07-10 16:46:35 -0300

    Main App configured, and new components and props created

--- Project 2/src/App.vue
+++ Project 2/src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <ul>
-
+    <learning-resources v-for="res in storedResources" 
+    :key="res.id" 
+    :title="res.title"
+    :description="res.description"
+    :link="res.link"
+    ></learning-resources>
   </ul>
 </template>
 
 
 <script>
+import LearningResources from './components/learning-resources/LearningResource.vue'
+
 export default {
+  components: {
+    LearningResources
+  },
   data() {
     return {
-      storeResources: [
+      storedResources: [
         {
           id: 'official-guide',
           title: 'Official Guide',
